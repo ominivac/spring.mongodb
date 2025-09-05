31d1938b16805b702c6f5cc8b0d3d3d02ac60bd2
ominivac <robertokbs@gmail.com>
2025-09-05 00:39:23 -0300

    update todo controller

--- src/main/java/org/rkbs/controller/TodoController.java
+++ src/main/java/org/rkbs/controller/TodoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,7 @@ public class TodoController {
 		}
 	}
 	
+	
 	@PostMapping("/todos")
 	public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDto){
 		
@@ -46,6 +48,28 @@ public class TodoController {
 		
 	}
 	
+	
+	@PutMapping("/todos/{id}")
+	public ResponseEntity<?> updateTodoById(@PathVariable("id") String id, @RequestBody TodoDTO todo){
+		Optional<TodoDTO> todoOptional = todoRepository.findById(id);
+		if(todoOptional.isPresent() ) {
+			TodoDTO todoToUpdate = todoOptional.get();
+			todoToUpdate.setCompleted(todo.getCompleted() != null ? todo.getCompleted() : todoToUpdate.getCompleted());
+			todoToUpdate.setTodo(todo.getTodo() !=null ? todo.getTodo() : todoToUpdate.getTodo() );
+			todoToUpdate.setDescription(todo.getDescription() != null ? todo.getDescription() : todoToUpdate.getDescription() );
+			todoToUpdate.setUpdatedAt( new Date(System.currentTimeMillis() ));
+			
+			todoRepository.save(todoToUpdate);
+			
+			return new ResponseEntity<>(todoToUpdate, HttpStatus.OK);
+		
+		}else {
+			return new ResponseEntity<>("Todo not found with id " + id, HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+
 	@GetMapping("/todos/{id}")
 	public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id){
 		Optional<TodoDTO> todoOptional = todoRepository.findById(id);
@@ -56,6 +80,9 @@ public class TodoController {
 		}
 		
 	}
+	
+	
+	
 	
 	
 	
