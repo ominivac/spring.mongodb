aeeac53e81d11a9d2ace23e4419b33e5ff6fd77d
ominivac <robertokbs@gmail.com>
2025-09-05 01:20:11 -0300

    delete metodo by id

--- src/main/java/org/rkbs/controller/TodoController.java
+++ src/main/java/org/rkbs/controller/TodoController.java
@@ -10,6 +10,7 @@ import org.rkbs.model.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,6 +78,18 @@ public class TodoController {
 			return new ResponseEntity<>(todoOptional.get() , HttpStatus.OK);
 		}else {
 			return new ResponseEntity<>("Todo not found with id ", HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@DeleteMapping("/todos/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable("id") String id) {
+		
+		try {
+			todoRepository.deleteById(id);
+			return new ResponseEntity<> ("Sucessfully delete with id " + id, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
 		
 	}
